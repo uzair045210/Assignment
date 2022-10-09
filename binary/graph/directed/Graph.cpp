85da555373ca199a7f13c37ec495a08aaa966bb1
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2022-10-08 22:41:30 -0700

    Directed Graphs - Complete

---
+++ binary/graph/directed/Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mr. Uzair on 10/6/2022.
+//
+
+#include "Graph.h"
---
+++ binary/graph/directed/Graph.h
@@ -0,0 +1,45 @@
+//
+// Created by Mr. Uzair on 10/6/2022.
+//
+
+#ifndef ASSIGNMENT_GRAPH_H
+#define ASSIGNMENT_GRAPH_H
+#include <map>
+#include <vector>
+#include <iostream>
+using namespace std;
+class Graph {
+public:
+//    map<int, vector<int>> *adj{};
+//
+//    void setAdj(int u, int v) {
+//        adj[u].push_back(v);
+//    }
+//
+//    void printAdj() {
+//        for(auto it=adj->begin(); it!=adj->end(); ++it)
+//            cout << it << endl;
+//            for(auto each: adj[it->first])
+//                cout << each << " ";
+//            cout << endl;
+//    }
+//
+//
+//    void use() {
+//        setAdj(1, 3);
+//        setAdj(2, 3);
+//        setAdj(2, 5);
+//        setAdj(2, 8);
+//        setAdj(2, 6);
+//        setAdj(5, 4);
+//        setAdj(6, 3);
+//        setAdj(8, 1);
+//        setAdj(9, 2);
+//        setAdj(0, 4);
+//
+//        printAdj();
+//    }
+};
+
+
+#endif //ASSIGNMENT_GRAPH_H
